7ca4e5c10723b3f9ed52100e78305fb1d39080c9
Vailton Gomes <governador.adm@gmail.com>
2020-06-24 20:22:08 -0300

    Atualização

---
+++ Aula7/Produto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula7
+{
+    public class Produto
+    {
+        public string Nome;
+
+        public double Preco;
+
+        public int Quantidade;
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = 15;
+        }
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        public Produto()
+        {
+        }
+        public double ValorEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarQuantidadeEstoque(int qtd)
+        {
+            Quantidade += qtd;
+        }
+
+        public void RemoverQuantidadeEstoque(int qtd)
+        {
+            Quantidade -= qtd;
+        }
+       
+    }
+}
--- Aula7/Program.cs
+++ Aula7/Program.cs
@@ -1,12 +1,65 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Aula7
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Nome do Produto:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Preço do produto:");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+
+            Console.WriteLine("Quantidade:");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            //Produto produto = new Produto();
+            Produto produto = new Produto("Arroz", 10.00, 0);
+            Produto produtoA = new Produto("Feijao", 8.00);
+            Produto produtoB = new Produto();
+            // dois produtos 
+            Console.WriteLine("Nome do Produto:");
+            produto.Nome = Console.ReadLine();
+
+            Console.WriteLine("Preço do produto:");
+            produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quantidade:");
+            produto.Quantidade = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Produto: " + produto);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Adicione mais produto " + produto.Nome + " no estoque: ");
+
+            int addQtd = int.Parse(Console.ReadLine());
+
+            produto.AdicionarQuantidadeEstoque(addQtd);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Total Atualizado: " + produto);
+
+            Console.WriteLine();
+
+            Console.WriteLine();
+
+
+            Console.WriteLine("Remova produto " + produto.Nome + " no estoque: ");
+
+            int rmvQtd = int.Parse(Console.ReadLine());
+
+            produto.RemoverQuantidadeEstoque(rmvQtd);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Total Atualizado: " + produto);
+
         }
     }
 }
